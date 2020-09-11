77ac47d0f1ad01a1260831eb50a45da2e3196197
slowdive <schlickjoe@gmail.com>
2020-09-11 12:55:02 -0700

    all files - cleaned up code and comments added

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+//Dependencies
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
 
+//Output
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
+//Stores employee objects
 let employeeArray = [];
 
-// let EmployeeArr = []
-// const Joe = new Engineer("joe", 15, "joe@yahoo.com", "joe's github")
-// EmployeeArr.push(Joe)
-// console.log(render(EmployeeArr))
 appMenu();
 
+//Starts inquirer 
 function appMenu() {
   inquirer
     .prompt([
@@ -34,32 +34,34 @@ function appMenu() {
           promptNewMember();
           break;
         case "Create a team":
-            createTeam();
-            break;
+          createTeam();
+          break;
         case "Exit":
           process.exit();
       }
     });
 }
+
+//Collects employee data
 function promptNewMember() {
   inquirer
     .prompt([
       {
         type: "input",
         name: "name",
-        message: "Enter your name",
+        message: "Enter employee name",
       },
 
       {
         type: "input",
         name: "id",
-        message: "Enter your id number",
+        message: "Enter employee id number",
       },
 
       {
         type: "input",
         name: "email",
-        message: "Enter your email address",
+        message: "Enter employee email address",
       },
     ])
     .then((newMember) => {
@@ -72,7 +74,7 @@ function promptNewMember() {
           {
             type: "list",
             name: "role",
-            message: "Select your role",
+            message: "Select employee role",
             choices: ["Manager", "Engineer", "Intern"],
           },
         ])
@@ -85,7 +87,7 @@ function promptNewMember() {
                   {
                     type: "input",
                     name: "officeNumber",
-                    message: "Enter your office number",
+                    message: "Enter manager office number",
                   },
                 ])
                 .then((managerChoice) => {
@@ -95,14 +97,12 @@ function promptNewMember() {
                     newMember.email,
                     managerChoice.officeNumber
                   );
-
                   const newManager = new Manager(
                     newMember.name,
                     newMember.id,
                     newMember.email,
                     managerChoice.officeNumber
                   );
-
                   employeeArray.push(newManager);
                   appMenu();
                 });
@@ -114,7 +114,7 @@ function promptNewMember() {
                   {
                     type: "input",
                     name: "github",
-                    message: "Enter your github ID",
+                    message: "Enter engineer github ID",
                   },
                 ])
                 .then((engineerChoice) => {
@@ -124,14 +124,12 @@ function promptNewMember() {
                     newMember.email,
                     engineerChoice.github
                   );
-
                   const newEngineer = new Engineer(
                     newMember.name,
                     newMember.id,
                     newMember.email,
                     engineerChoice.github
                   );
-
                   employeeArray.push(newEngineer);
                   appMenu();
                 });
@@ -143,7 +141,7 @@ function promptNewMember() {
                   {
                     type: "input",
                     name: "school",
-                    message: "Enter your school",
+                    message: "Enter intern school",
                   },
                 ])
                 .then((internChoice) => {
@@ -153,14 +151,12 @@ function promptNewMember() {
                     newMember.email,
                     internChoice.school
                   );
-
                   const newIntern = new Intern(
                     newMember.name,
                     newMember.id,
                     newMember.email,
                     internChoice.school
                   );
-
                   employeeArray.push(newIntern);
                   appMenu();
                 });
@@ -170,40 +166,14 @@ function promptNewMember() {
     });
 }
 
+//Creates HTML
 function createTeam() {
-    let htmlFile = render(employeeArray)
-    fs.writeFile(outputPath, htmlFile, function(err) {
-        if (err) {
-            return console.log(err);
-        } else {
-            console.log("\x1b[32m","Success! -- File Created");
-        }
-        });
+  let htmlFile = render(employeeArray);
+  fs.writeFile(outputPath, htmlFile, function (err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("\x1b[32m", "Success! -- File Created");
+    }
+  });
 }
-
-//function appMenu() inside - create one Manager. Create manager first, then createTeam() function , in createteam have a switch case to ask which kind of emmployee needs to be created and then run the needed function for createEngineer or createIntern if there is no other entry then build the team and render.
-
-// in each function for employees ask Qs for data and then in .then construct employee with data given
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,12 +1,10 @@
 const Employee = require("../lib/Employee");
 
-
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
-
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
@@ -39,9 +37,9 @@ test("Can get email via getEmail()", () => {
 });
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(typeof(e)).toBe("object");
+  expect(typeof e).toBe("object");
 });
-test("getRole() should return \"Employee\"", () => {
+test('getRole() should return "Employee"', () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
